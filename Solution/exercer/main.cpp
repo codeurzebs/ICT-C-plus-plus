d38d738663d15407a17870ae5b8e133bb0e83ebe
NGUENAZEBS <ngcodex@gmail.com>
2023-05-25 00:18:44 +0100

    Update main.cpp

--- Solution/exercer/main.cpp
+++ Solution/exercer/main.cpp
@@ -1,3 +1,16 @@
+/* @Codeur ZEBS       
+ * Repos  04/05/@2023
+ * @IDE      CodeBlocks
+ * @univ 	    ICT
+ * @copyright       Copyright (c) 2022 ZEBS
+ * @license         My Login is licensed under the CodeBlocks license.
+ * @github          https://github.com/NGUENAZEBS
+ * @version         1.0.1 ICT-U
+ * Help me to keep this project
+ *https://ko-fi.com/pilotezebs
+ *club      NGcodeX
+ */
+
 #include <iostream>
 #include <string.h>
 #include <math.h>
@@ -81,4 +94,15 @@ int main()
     return 0;
 }
 
-
+/* @Codeur ZEBS       
+ * Repos  04/05/@2023
+ * @IDE      CodeBlocks
+ * @univ 	    ICT
+ * @copyright       Copyright (c) 2022 ZEBS
+ * @license         My Login is licensed under the CodeBlocks license.
+ * @github          https://github.com/NGUENAZEBS
+ * @version         1.0.1 ICT-U
+ * Help me to keep this project
+ *https://ko-fi.com/pilotezebs
+ *club      NGcodeX
+ */
