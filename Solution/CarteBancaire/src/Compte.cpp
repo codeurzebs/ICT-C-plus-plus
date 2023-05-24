e2343859ee85e47d8e064e92b2a6d1f4406b34a6
NGUENAZEBS <ngcodex@gmail.com>
2023-05-25 00:16:02 +0100

    Update Compte.cpp

--- Solution/CarteBancaire/src/Compte.cpp
+++ Solution/CarteBancaire/src/Compte.cpp
@@ -1,3 +1,17 @@
+/* @Codeur ZEBS       
+ * Repos Presentation 25/05/23
+ * ONLINE
+ * @IDE      CodeBlocks
+ * @univ 	    ICT
+ * @copyright       Copyright (c) 2022 ZEBS
+ * @license         My Login is licensed under the ECLIPSE license.
+ * @github          https://github.com/NGUENAZEBS
+ * @version         1.0.1 ICT-U
+ * Help me to keep this project
+ *https://ko-fi.com/pilotezebs
+ *club      NGcodeX
+ */
+
 #include "Compte.h"
 #include "Client.h"
 #include<iostream>
@@ -57,3 +71,18 @@ Compte::~Compte()
 {
     //dtor
 }
+
+
+/* @Codeur ZEBS       
+ * Repos Presentation 25/05/23
+ * ONLINE
+ * @IDE      CodeBlocks
+ * @univ 	    ICT
+ * @copyright       Copyright (c) 2022 ZEBS
+ * @license         My Login is licensed under the ECLIPSE license.
+ * @github          https://github.com/NGUENAZEBS
+ * @version         1.0.1 ICT-U
+ * Help me to keep this project
+ *https://ko-fi.com/pilotezebs
+ *club      NGcodeX
+ */
